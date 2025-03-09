aeee8544e29a72f4b849fce367c6dd2456ef05c5
CharmedGoose <goose@goosehub.net>
2025-03-08 19:02:16 -0500

    feat: add ping command

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+import { SapphireClient } from "@sapphire/framework";
+import { GatewayIntentBits } from "discord.js";
+
+const client = new SapphireClient({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+});
+
+client.login(process.env.TOKEN);
