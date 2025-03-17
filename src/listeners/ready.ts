f373d25df542189bc96ad1557216593242f04f0a
CharmedGoose <goose@goosehub.net>
2025-03-17 19:07:24 -0400

    refactor: update ready.ts

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -11,15 +11,13 @@ export class ReadyListener extends Listener {
 			event: "ready",
 		});
 	}
-	public run() {
-		this.container.logger.info(
-			String.raw` ____       _        _     _            
-|  _ \ ___ | | _____| |__ | |_   _  ___ 
-| |_) / _ \| |/ / _ \ '_ \| | | | |/ _ \
-|  __/ (_) |   <  __/ |_) | | |_| |  __/
-|_|   \___/|_|\_\___|_.__/|_|\__,_|\___| ${process.env.DEBUG ? "DEBUG MODE" : ""}
-`,
-		);
+	public async run() {
+		this.container.logger.info(String.raw` ____       _        _     _             `); // prettier-ignore
+		this.container.logger.info(String.raw`|  _ \ ___ | | _____| |__ | |_   _  ___  `); // prettier-ignore
+		this.container.logger.info(String.raw`| |_) / _ \| |/ / _ \ '_ \| | | | |/ _ \ `); // prettier-ignore
+		this.container.logger.info(String.raw`|  __/ (_) |   <  __/ |_) | | |_| |  __/ `); // prettier-ignore
+		this.container.logger.info(String.raw`|_|   \___/|_|\_\___|_.__/|_|\__,_|\___| ${process.env.NODE_ENV === "production" ? "Production" : "Development"}`,); // prettier-ignore
+
 		this.container.logger.debug("hai!");
 	}
 }
